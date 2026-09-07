df1010d71220599946cc22a7deb2254afebaf2ca
kakerusaman <kakerusaman@gmail.com>
2026-09-08 02:16:05 +0900

    環境変数用のモジュールとDB操作系のモジュールの作成

---
+++ kakerusaman-customer/customer-web/src/main/java/com/github/kakerusaman/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.github.kakerusaman.login;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+	
+	//@GetMapping(value = "/")
+	//public String hoge() {
+	//	return "index";
+	//}
+}
