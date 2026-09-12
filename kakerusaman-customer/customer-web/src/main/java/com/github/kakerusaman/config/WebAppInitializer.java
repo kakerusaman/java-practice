7f575581251615bcd76904fb5a53738b961971cb
kakerusaman <kakerusaman@gmail.com>
2026-09-12 20:47:03 +0900

    jspを表示できるように修正

--- kakerusaman-customer/customer-web/src/main/java/com/github/kakerusaman/config/WebAppInitializer.java
+++ kakerusaman-customer/customer-web/src/main/java/com/github/kakerusaman/config/WebAppInitializer.java
@@ -3,8 +3,10 @@ package com.github.kakerusaman.config;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
@@ -13,10 +15,15 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		System.out.println("=== WebAppInitializer.onStartup() 開始 ===");
 		try {
 			AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+			// DBConfig
 			context.register(DataSourceConfig.class);
-			System.out.println("=== DataSourceConfig 登録完了 ===");
-			servletContext.addListener(new ContextLoaderListener(context));
-			System.out.println("=== ContextLoaderListener 登録完了 ===");
+			
+			// WebAPの設定
+            context.register(WebConfig.class);
+			ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+			dispatcher.setLoadOnStartup(1);
+			dispatcher.addMapping("/");
+
 		} catch (Throwable t) {
 			System.err.println("=== WebAppInitializer エラー: " + t.getMessage());
 			t.printStackTrace(System.err);
