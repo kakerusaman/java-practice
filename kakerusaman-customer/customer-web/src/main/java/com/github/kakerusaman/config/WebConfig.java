d0daa12fe873f04257fb8b3aa5ab61b7eb2ea6a4
kakerusaman <kakerusaman@gmail.com>
2026-09-12 20:47:32 +0900

    jspのConfigファイル

---
+++ kakerusaman-customer/customer-web/src/main/java/com/github/kakerusaman/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.github.kakerusaman.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration 
+@EnableWebMvc 
+@ComponentScan(basePackages = "com.github.kakerusaman.controller")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean 
+    public InternalResourceViewResolver vviewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
